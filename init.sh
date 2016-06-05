f89ceda686002b3c8ec972ea985b5d8edadb6e33
ShaunH <shaun@hedlabs.com>
2016-06-05 19:41:57 -0400

    call echo with sudo bash

--- init.sh
+++ init.sh
@@ -24,7 +24,7 @@ sudo apt-get -y install cppcheck
 # Docker
 sudo apt-get -y install apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list
+sudo bash -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list"
 sudo apt-get update
 sudo apt-get purge lxc-docker
 sudo apt-get -y install linux-image-extra-$(uname -r)
