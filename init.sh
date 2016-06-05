26f7a7565609a42fc00ec8efcf67d2966dcab9af
ShaunH <shaun@hedlabs.com>
2016-06-05 19:47:27 -0400

    reorganize for fewer apt-get updates

--- init.sh
+++ init.sh
@@ -10,8 +10,13 @@ sudo apt-get -y install openjdk-7-jdk
 # Add Jenkins to apt-get sources list
 sudo wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+# Add Docker to apt-get sources list
+sudo apt-get -y install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo sh -c 'echo deb https://apt.dockerproject.org/repo ubuntu-trusty main > /etc/apt/sources.list.d/docker.list'
+# Update with new apt-get source lists
 sudo apt-get update
-# CI Server
+# Jenkins CI Server
 sudo apt-get -y install jenkins
 # Source control management
 sudo apt-get -y install git
@@ -22,14 +27,9 @@ sudo apt-get -y install pylint
 # C/C++ static analysis
 sudo apt-get -y install cppcheck
 # Docker
-sudo apt-get -y install apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo bash -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list"
-sudo apt-get update
 sudo apt-get purge lxc-docker
 sudo apt-get -y install linux-image-extra-$(uname -r)
 sudo apt-get -y install apparmor
-sudo apt-get update
 sudo apt-get -y install docker-engine
 sudo service docker start
 
